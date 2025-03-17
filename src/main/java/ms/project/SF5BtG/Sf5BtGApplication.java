7f2f58448afffaa8276a0d0ee198795920f9d2f7
WeirdBlob <maciejstawiarz1@gmail.com>
2025-03-17 20:17:33 +0100

    Setting up the project

---
+++ src/main/java/ms/project/SF5BtG/Sf5BtGApplication.java
@@ -0,0 +1,12 @@
+package ms.project.SF5BtG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sf5BtGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sf5BtGApplication.class, args);
+	}
+}
---
+++ src/test/java/ms/project/SF5BtG/Sf5BtGApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.project.SF5BtG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sf5BtGApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
