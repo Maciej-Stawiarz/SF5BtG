da3fb37f4bb9b6c946f3618d9237c15484a6fb22
Maciej-Stawiarz <precariousDuck@protonmail.com>
2025-03-26 12:42:36 +0100

    Changed application name

--- src/main/java/ms/project/SF5BtG/Spring5ClassicApplication.java
+++ src/main/java/ms/project/SF5BtG/Spring5ClassicApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Sf5BtGApplication {
+public class Spring5ClassicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Sf5BtGApplication.class, args);
+		SpringApplication.run(Spring5ClassicApplication.class, args);
 	}
 }
--- src/test/java/ms/project/SF5BtG/Spring5ClassicApplicationTests.java
+++ src/test/java/ms/project/SF5BtG/Spring5ClassicApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Sf5BtGApplicationTests {
+class Spring5ClassicApplicationTests {
 
 	@Test
 	void contextLoads() {
