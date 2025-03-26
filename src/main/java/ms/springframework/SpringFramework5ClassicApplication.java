d56a0199d2e0387b539c86c60aca19bf3e215712
Maciej-Stawiarz <precariousDuck@protonmail.com>
2025-03-26 12:52:12 +0100

    Changed package name

--- src/main/java/ms/springframework/SpringFramework5ClassicApplication.java
+++ src/main/java/ms/springframework/SpringFramework5ClassicApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring5ClassicApplication {
+public class SpringFramework5ClassicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5ClassicApplication.class, args);
+		SpringApplication.run(SpringFramework5ClassicApplication.class, args);
 	}
 }
